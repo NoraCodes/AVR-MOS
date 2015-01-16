9201f8d0c4a1a1ebae98f81eb8e5e7db2f422a35
SilverWingedSeraph <evessarudem@gmail.com>
2015-01-16 12:42:59 -0800

    Added "clean" target.

--- makefile.mk
+++ makefile.mk
@@ -1,6 +1,9 @@
 #Makefile for AVR-MOS
 project:
-	avr-gcc main.c -o PROJECT.o
+	avr-gcc main.c -o PROJECT.out
 	
 kernel:
-	avr-gcc kernel.c -o KERNEL.o
+	avr-gcc kernel.c -o KERNEL.out
+	
+clean:
+	rm -rf *.o *.out
