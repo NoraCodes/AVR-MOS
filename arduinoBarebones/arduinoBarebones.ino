87d52a10489b640db7b0a8f20de18f2d8723892e
SilverWingedSeraph <evessarudem@gmail.com>
2015-01-16 07:11:14 -0800

    Added barebones Arduino sketch.

---
+++ arduinoBarebones/arduinoBarebones.ino
@@ -0,0 +1,17 @@
+/*
+A barebones sketch using AVR MOS.
+*/
+
+char userInit(void){
+  //Init stuff
+  return 0;
+}
+
+char userLoop(void){
+  //Loop stuff
+  return 0; 
+}
+
+void userPanic(void){
+  //Panic stuff
+}
